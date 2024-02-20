06cc999c657d96166e3474caa68c442d649103e3
ro11ast <113983706+ro11ast@users.noreply.github.com>
2024-02-20 16:56:30 +0000

    changes

--- lib/ar_view.dart
+++ lib/ar_view.dart
@@ -1,3 +1,4 @@
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
 import 'package:ar_flutter_plugin/datatypes/hittest_result_types.dart';
 import 'package:ar_flutter_plugin/datatypes/node_types.dart';
@@ -5,12 +6,11 @@ import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin/models/ar_anchor.dart';
 import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
 import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/material.dart';
-import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
-import 'package:vector_math/vector_math_64.dart' as vector;
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class ARViewPage extends StatefulWidget {
   const ARViewPage({super.key});
@@ -27,66 +27,96 @@ class _ARViewPageState extends State<ARViewPage> {
 
   @override
   void dispose() {
-    arSessionManager?.dispose();
     super.dispose();
+    arSessionManager!.dispose();
   }
+
+  List<ARNode> nodes = [];
+  List<ARAnchor> anchors = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("AR View"),
-      ),
       body: ARView(
         onARViewCreated: _onARViewCreated,
         planeDetectionConfig: PlaneDetectionConfig.horizontalAndVertical,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addARObject,
-        tooltip: 'Add AR Object',
-        child: const Icon(Icons.add),
-      ), // This trailing comment was missing a closing bracket in your original snippet
     );
   }
 
-  void _onARViewCreated(ARSessionManager arSessionManager, ARObjectManager arObjectManager, ARAnchorManager arAnchorManager, ARLocationManager arLocationManager) {
+  void _onARViewCreated(
+      ARSessionManager arSessionManager,
+      ARObjectManager arObjectManager,
+      ARAnchorManager arAnchorManager,
+      ARLocationManager arLocationManager) {
     this.arSessionManager = arSessionManager;
     this.arObjectManager = arObjectManager;
     this.arAnchorManager = arAnchorManager;
     this.arLocationManager = arLocationManager;
 
-    arSessionManager.onPlaneOrPointTap = _onPlaneTapped;
+    this.arSessionManager!.onPlaneOrPointTap = onPlaneOrPointTapped;
+
+    this.arSessionManager!.onInitialize(
+        showFeaturePoints: false,
+        showPlanes: true,
+        customPlaneTexturePath: "Images/triangle.png",
+        showWorldOrigin: false,
+        showAnimatedGuide: true);
+    this.arObjectManager!.onInitialize();
+
+    this.arObjectManager!.onNodeTap = onNodeTapped;
+
+
   }
 
-  void _onPlaneTapped(List<ARHitTestResult> hitTestResults) {
-    for (var hitTestResult in hitTestResults) {
-      if (hitTestResult.type == ARHitTestResultType.plane) {
-        _addARObjectAtHit(hitTestResult);
-        return;
+  Future<void> onNodeTapped(List<String> nodes) async {
+    var number = nodes.length;
+    arSessionManager!.onError("Tapped $number node(s)");
+  }
+
+  Future<void> onPlaneOrPointTapped(
+      List<ARHitTestResult> hitTestResults) async {
+    var singleHitTestResult = hitTestResults.firstWhere(
+            (hitTestResult) => hitTestResult.type == ARHitTestResultType.plane);
+    var newAnchor =
+    ARPlaneAnchor(transformation: singleHitTestResult.worldTransform);
+    bool? didAddAnchor = await arAnchorManager!.addAnchor(newAnchor);
+    if (didAddAnchor!) {
+      anchors.add(newAnchor);
+      // Add note to anchor
+      var newNode1 = ARNode(
+        type: NodeType.webGLB,
+        uri: "assets/models/Monkey.glb",
+        scale: Vector3(0.2, 0.2, 0.2),
+        position: Vector3(0.0, 0.0, 0.0),
+        rotation: Vector4(1.0, 0.0, 0.0, 0.0),
+      );
+
+      var newNode2 = ARNode(
+        type: NodeType.webGLB,
+        uri: "assets/models/AnotherModel.glb",
+        scale: Vector3(0.2, 0.2, 0.2),
+        position: Vector3(0.0, 0.0, 0.0),
+        rotation: Vector4(1.0, 0.0, 0.0, 0.0),
+      );
+
+      bool? didAddNodeToAnchor1 =
+      await arObjectManager!.addNode(newNode1, planeAnchor: newAnchor);
+      if (didAddNodeToAnchor1!) {
+        nodes.add(newNode1);
+      } else {
+        arSessionManager!.onError("Adding Node to Anchor failed");
       }
+
+      bool? didAddNodeToAnchor2 =
+      await arObjectManager!.addNode(newNode2, planeAnchor: newAnchor);
+      if (didAddNodeToAnchor2!) {
+        nodes.add(newNode2);
+      } else {
+        arSessionManager!.onError("Adding Node to Anchor failed");
+      }
+    } else {
+      arSessionManager!.onError("Adding Anchor failed");
     }
-  }
-
-  void _addARObject() async {
-    final cacheManager = DefaultCacheManager();
-    final file = await cacheManager.getSingleFile('assets/models/Monkey.glb');
-
-    final node = ARNode(
-      type: NodeType.webGLB,
-      uri: file.path,
-      position: vector.Vector3(0, 0, -1),
-      scale: vector.Vector3.all(0.5),
-    );
-    arObjectManager?.addNode(node);
-  }
-
-  void _addARObjectAtHit(ARHitTestResult hitTestResult) {
-    final node = ARNode(
-      type: NodeType.webGLB,
-      uri: "assets/models/Monkey.glb",
-      position: vector.Vector3(0, 0, -1),
-      scale: vector.Vector3.all(0.5),
-    );
-    arObjectManager?.addNode(node);
   }
 }
--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        title: const Text('Settings'),
       ),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
