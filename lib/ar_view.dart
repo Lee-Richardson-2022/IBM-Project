13f0a182ca79042c43485349571813b2453eefc6
Lee Richardson <114064832+Lee-Richardson-2022@users.noreply.github.com>
2024-02-22 15:15:27 +0000

    Add files via upload

--- lib/ar_view.dart
+++ lib/ar_view.dart
@@ -14,18 +14,23 @@ class _ARViewPageState extends State<ARViewPage> {
   ArCoreNode? currentNode;
   double avatarScale = 1.0;
 
+
+void _onBottonNavTap(int index) {
+  if (index == 2) {
+    _showSettingsDialog();
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('AR View'),
-      ),
       body: Column(
         children: [
           Expanded(
             child: ArCoreView(
               onArCoreViewCreated: _onArCoreViewCreated,
               enableTapRecognizer: true,
+              enableUpdateListener: true,
             ),
           ),
           BottomNavigationBar(
@@ -43,17 +48,11 @@ class _ARViewPageState extends State<ARViewPage> {
                 label: 'Settings',
               ),
             ],
-            onTap: _onBottomNavTap,
+            onTap: _onBottonNavTap,
           ),
         ],
       ),
     );
-  }
-
-  void _onBottomNavTap(int index) {
-    if (index == 2) {
-      _showSettingsDialog();
-    }
   }
 
   void _onArCoreViewCreated(ArCoreController controller) {
@@ -68,6 +67,39 @@ class _ARViewPageState extends State<ARViewPage> {
     }
   }
 
+  List<ArCoreNode> createButtons() {
+    final redMaterial = ArCoreMaterial(color: Colors.red, reflectance: 1.0);
+    final redCylinder = ArCoreCylinder(materials: [redMaterial], radius: 0.01, height: 0.05);
+
+    final blueMaterial = ArCoreMaterial(color: Colors.blue, reflectance: 1.0);
+    final blueCylinder = ArCoreCylinder(materials: [blueMaterial], radius: 0.01, height: 0.05);
+
+    final greenMaterial = ArCoreMaterial(color: Colors.green, reflectance: 1.0);
+    final greenCylinder = ArCoreCylinder(materials: [greenMaterial], radius: 0.01, height: 0.05);
+
+    final redButton = ArCoreNode(
+      name: 'redButton',
+      shape: redCylinder,
+      position: vector.Vector3(-0.1, 0, 0),
+      rotation: vector.Vector4(1, 0, 0, 1),
+    );
+
+    final blueButton = ArCoreNode(
+      name: 'blueButton',
+      shape: blueCylinder,
+      position: vector.Vector3(0.1, 0, 0),
+      rotation: vector.Vector4(1, 0, 0, 1),
+    );
+
+    final greenButton = ArCoreNode(
+      name: 'greenButton',
+      shape: greenCylinder,
+      position: vector.Vector3(0, 0.1, 0),
+      rotation: vector.Vector4(1, 0, 0, 1),
+    );
+    return [redButton, blueButton, greenButton];
+  }
+
   void createAvatar(vector.Vector3 position, Color color, bool withButtons) {
     final material = ArCoreMaterial(color: color, reflectance: 1.0);
     final cylinder = ArCoreCylinder(materials: [material], radius: 0.15, height: 0.03);
@@ -78,14 +110,14 @@ class _ARViewPageState extends State<ARViewPage> {
         children: createButtons(),
         shape: cylinder,
         position: position,
-        scale: vector.Vector3.all(avatarScale),
+        scale: vector.Vector3(avatarScale, avatarScale, avatarScale),
       );
     } else {
       newNode = ArCoreNode(
         name: 'avatar',
         shape: cylinder,
         position: position,
-        scale: vector.Vector3.all(avatarScale),
+        scale: vector.Vector3(avatarScale, avatarScale, avatarScale),
       );
     }
 
@@ -97,27 +129,16 @@ class _ARViewPageState extends State<ARViewPage> {
     currentNode = newNode;
   }
 
-  List<ArCoreNode> createButtons() {
-    // Define materials
-    final redMaterial = ArCoreMaterial(color: Colors.red, reflectance: 1.0);
-    final blueMaterial = ArCoreMaterial(color: Colors.blue, reflectance: 1.0);
-    final greenMaterial = ArCoreMaterial(color: Colors.green, reflectance: 1.0);
-
-    // Create cylinders with defined materials
-    final redCylinder = ArCoreCylinder(materials: [redMaterial], radius: 0.01, height: 0.05);
-    final blueCylinder = ArCoreCylinder(materials: [blueMaterial], radius: 0.01, height: 0.05);
-    final greenCylinder = ArCoreCylinder(materials: [greenMaterial], radius: 0.01, height: 0.05);
-
-    // Assign nodes for each color
-    final redButton = ArCoreNode(shape: redCylinder, position: vector.Vector3(-0.1, 0, 0));
-    final blueButton = ArCoreNode(shape: blueCylinder, position: vector.Vector3(0.1, 0, 0));
-    final greenButton = ArCoreNode(shape: greenCylinder, position: vector.Vector3(0, 0.1, 0));
-
-    return [redButton, blueButton, greenButton];
+  void changeColor(Color color) {
+    arCoreController.removeNode(nodeName: currentNode!.name);
+    createAvatar(currentNode!.position!.value, color, true);
   }
 
   void onTapHandler(String name) {
     switch (name) {
+      case "avatar":
+        createAvatar(currentNode!.position!.value, Colors.grey, true);
+        break;
       case "redButton":
         changeColor(Colors.red);
         break;
@@ -130,14 +151,9 @@ class _ARViewPageState extends State<ARViewPage> {
     }
   }
 
-  void changeColor(Color color) {
-    if (currentNode != null) {
-      createAvatar(currentNode!.position!.value, color, true);
-    }
-  }
-
   void _updateAvatarScale() {
     if (currentNode != null) {
+      arCoreController.removeNode(nodeName: currentNode!.name);
       createAvatar(currentNode!.position!.value, Colors.grey, true);
     }
   }
@@ -146,34 +162,58 @@ class _ARViewPageState extends State<ARViewPage> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
+        bool subtitlesEnabled = false; // Variable to track subtitles option
+        double avatarSize = avatarScale; // Variable to track avatar size
+
         return StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
             return AlertDialog(
+              backgroundColor: Colors.white60, // Set background color to transparent
+              elevation: 0, // Set elevation to 0 to remove shadow
               title: const Text("Settings"),
               content: SingleChildScrollView(
                 child: ListBody(
                   children: <Widget>[
-                    Text('Avatar Size: ${avatarScale.toStringAsFixed(2)}'),
+                    // Subtitles option
+                    CheckboxListTile(
+                      title: const Text('Subtitles'),
+                      value: subtitlesEnabled,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          subtitlesEnabled = value ?? true;
+                          // Implement subtitle display logic based on the value
+                          // For example: if (subtitlesEnabled) { /* Show subtitles */ }
+                        });
+                      },
+                    ),
+                    // Avatar size label and slider
+
+                    Text(
+                      'Avatar Size: ${avatarSize.toStringAsFixed(2)}', // Display current avatar size
+                      style: TextStyle(fontSize: 16),
+                    ),
                     Slider(
-                      value: avatarScale,
+                      value: avatarSize,
                       min: 0.5,
                       max: 2.0,
-                      divisions: 15,
-                      label: avatarScale.toStringAsFixed(2),
-                      onChanged: (double value) {
+                      onChanged: (value) {
                         setState(() {
-                          avatarScale = value;
-                          _updateAvatarScale();
+                          avatarSize = value;
+                          avatarScale = value; // Update the global avatarScale variable
+                          _updateAvatarScale(); // Update avatar scale in AR view
                         });
                       },
                     ),
                   ],
                 ),
               ),
+
               actions: <Widget>[
                 TextButton(
                   child: const Text('Close'),
-                  onPressed: () => Navigator.of(context).pop(),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
                 ),
               ],
             );
